6f9bff9a52812a4c4a4775ab899bb12156e311ad
thanh <leodomsolar@gmail.com>
2022-09-21 10:11:41 +0700

    Update index.blade.php

--- resources/views/Desktop/users/index.blade.php
+++ resources/views/Desktop/users/index.blade.php
@@ -132,7 +132,7 @@
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
-                <button type="button" class="btn btn-primary">Lưu</button>
+                <button type="button" class="btn btn-primary" id="addUserRolebtn">Lưu</button>
               </div>
             </div>
           </div>
